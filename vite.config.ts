25bfdbd4c391c5043813d6589711d29667695819
Yossef Gartner <yossefgartner@Yossefs-MacBook-Pro.local>
2025-06-05 11:10:50 +0300

    add dev build option

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,32 @@
 import { defineConfig } from 'vite'
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 
-export default defineConfig({
-  plugins: [
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'manifest.json',
-          dest: '.',
-        }
-      ],
-    }),
-  ],
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: './sidepanel/index.html',
+export default defineConfig(({ mode }) => {
+  const isDev = mode === 'dev' || process.env.NODE_ENV === 'development';
+  return {
+    plugins: [
+      viteStaticCopy({
+        targets: [
+          {
+            src: 'manifest.json',
+            dest: '.',
+          }
+        ],
+      }),
+    ],
+    build: {
+      outDir: 'dist',
+      minify: !isDev,
+      rollupOptions: {
+        input: {
+          main: './sidepanel/index.html',
+        },
+        output: isDev ? {
+          entryFileNames: '[name].js',
+          chunkFileNames: '[name].js',
+          assetFileNames: '[name][extname]',
+        } : undefined,
       },
     },
-  },
+  };
 });
